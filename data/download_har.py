824ba440752372bb8082fa8b8fa4637bcb992eb9
ChaitIITB <23b2712@iitb.ac.in>
2025-11-10 15:19:43 +0530

    Added the dataset

--- data/download_har.py
+++ data/download_har.py
@@ -1,6 +1,7 @@
+from datasets import load_dataset
 import pandas as pd
-# Load the dataset
-url = "http://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip"
-data = pd.read_csv(url, compression='zip', header=0, sep=',', quotechar='"')
-# Display the first few rows of the dataset
-print(data.head())
+
+ds = load_dataset("Bingsu/Human_Action_Recognition")
+print("Dataset Loaded")
+
+print(f"ds[train] is {ds["train"][0]["image"]}")
